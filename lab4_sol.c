becfdcab08f1e22c8f376485ef6da2c579ac2a70
Andrea McIntosh <akmcinto@ualberta.ca>
2016-03-10 14:57:04 -0700

    Make it work Jenna

--- lab4_sol.c
+++ lab4_sol.c
@@ -3,44 +3,80 @@
 #include <math.h>
 #include "Lab4_IO.h"
 
-int *n;
+#define EPSILON 0.00001
+#define DAMPING_FACTOR 0.85
 
 typedef struct {
-    int Di[1500];
-    int size_Di;
-    int li;
-    double ri;
+    int Di[1500]; // Set of nodes with an outgoing link to this node
+    int size_Di;  // Number of nodes with outgoing link to this node
+    int li;       // Number of outgoing links from this node
 } node;
 
+double* calculate(double *r, node *A);
+int n; // number of nodes
+
+
 void main(void) {
-    FILE* ip;
+    FILE *ip;
     int i,j;
+    int src, dest;
+    node *A;        // node data struct
+    double *R;      // result
 
     if ((ip = fopen("data_input","r")) == NULL){
-            printf("error opening the input data.\n");
+        printf("error opening the input data.\n");
     }
-    fscanf(ip, "%d\n\n", n);
+    fscanf(ip, "%d\n", &n);
 
-    node *A;
-    A = malloc(*n * sizeof(node));
-    for (i = 0; i < *n; i++) {
+    R = malloc(n * sizeof(double));
+    for (i = 0; i < n; i++) {
+        R[i] = 1.0 / n;
+    }
+
+    A = malloc(n * sizeof(node));
+    for (i = 0; i < n; i++) {
         A[i].li = 0;
         A[i].size_Di = 0;
-        A[i].ri = 1 / *n;
     }
 
-    int src, dest;
-    for (i = 0; i < *n; ++i){
-        fscanf(ip, "%d %d\t", src, dest);
+    for (i = 0; i < n; ++i){
+        fscanf(ip, "%d\t%d\n", &src, &dest);
         A[dest].Di[A[dest].size_Di] = src;
         (A[dest].size_Di)++;
         (A[src].li)++;
-
-        fscanf(ip, "\n");
     }
 
-    for (i = 0; i < *n; ++i){
-            printf("%d %d %d %f", A[i].Di[(A[i].size_Di)-1], A[i].size_Di, A[i].li, A[i].ri);
+    R = calculate(R, A);
 
+    for (i = 0; i < n; ++i){
+        printf("%f ", R[i]);
     }
+    printf("\n");
+
+    free(A); free(R);
+}
+
+double* calculate(double *r, node *A) {
+    int i, j;
+    double *r_pre;
+    r_pre = malloc(n * sizeof(double));
+    double damp_const = (1.0 - DAMPING_FACTOR) / n;
+
+    while(rel_error(r, r_pre, n) >= EPSILON) {
+        // Save previous values
+        for (i = 0; i < n; ++i) {
+            r_pre[i] = r[i];
+        }
+        for ( i = 0; i < n; ++i){
+            r[i] = 0;
+            for ( j = 0; j < A[i].size_Di; ++j)
+                r[i] += r_pre[A[i].Di[j]] / A[A[i].Di[j]].li;
+            r[i] *= DAMPING_FACTOR;
+            r[i] += damp_const;
+        }
+    }
+
+    free(r_pre);
+
+    return r;
 }
